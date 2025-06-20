37543b55b09f21119238661e4450fca410e824fd
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-06-20 02:44:03 -0300

    feat: Criando rota para buscar dados personalizados do pokemon

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,5 +11,27 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  getPokemons(limit: number = 50, offset: number = 0): Observable<any[]> {
+    // Request geral para buscar a lista de pokemons da página
+    return this.http.get<any[]>(`${this.apiUrl}/pokemon?limit=${limit}&offset=${offset}`).pipe(
+      switchMap((response: any) => {
+        // Busca a url de cada pokemon para buscar os dados de cada um
+        const requests = response.results.map((pokemon: any) => {
+          return this.http.get<any>(pokemon.url);
+        });
+        // Realiza um fork para requisições em paralelo aos dados de cada pokemon
+        return forkJoin(requests).pipe(
+          map((pokemonDetails: any) => {
+            // Retorna os dados do pokemon
+            return pokemonDetails.map((detail: any) => ({
+              id: detail.id,
+              name: detail.name,
+              image: detail.sprites.front_default
+            }));
+          })
+        )
+      })
+    );
+  }
   
 }

169a3481b9aa7a31ee33b125f4d84532044d8251
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-06-20 11:16:10 -0300

    feat: Criando funcionalidade de paginação dos pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,17 +3,20 @@ import { IonContent } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
 import { CommonModule } from '@angular/common';
 import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.component';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, CommonModule, PokemonCardComponent],
+  imports: [IonContent, CommonModule, PokemonCardComponent, MatPaginatorModule],
 })
 export class HomePage implements OnInit {
 
   totalPokemons: number = 0;
   pokemons: any[] = [];
+  pageSize: number = 10;
+  currentPage: number = 0;
 
   constructor(private dataService: DataService) {}
 
@@ -22,7 +25,8 @@ export class HomePage implements OnInit {
   }
 
   getPokemons() {
-    this.dataService.getPokemons().subscribe({
+    const offset = this.currentPage * this.pageSize;
+    this.dataService.getPokemons(this.pageSize, offset).subscribe({
       next: (data: any) => {
         this.totalPokemons = data.total,
         this.pokemons = data.pokemons
@@ -30,8 +34,13 @@ export class HomePage implements OnInit {
       error: (error) => {
         console.error("Erro ao buscar os pokemons: ", error);
       }
-    })
+    });
   }
 
+  pageChanged(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.getPokemons();
+  }
 
 }

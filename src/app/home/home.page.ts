0358b8b462e7f399d1e0bfbf1bd30210c865425a
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-06-20 02:44:29 -0300

    feat: Adicionando chamada da função de buscar pokemons na página home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
+import { DataService } from '../services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent],
+  imports: [IonContent, CommonModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  pokemons: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.getPokemons();
+  }
+
+  getPokemons() {
+    this.dataService.getPokemons().subscribe({
+      next: (data: any) => {
+        this.pokemons = data;
+      },
+      error: (error) => {
+        console.error("Erro ao buscar os pokemons: ", error);
+      }
+    })
+  }
+
+
 }

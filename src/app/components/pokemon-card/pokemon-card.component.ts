23714015d34145c41c1cbf4c79b567c3e1acbba9
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-06-20 02:57:38 -0300

    feat: Importando card component de pokemon na home

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class PokemonCardComponent  implements OnInit {
 
   constructor() { }
+
+  @Input() id!: number;
+  @Input() name!: number;
+  @Input() image!: number;
 
   ngOnInit() {}
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
 import { CommonModule } from '@angular/common';
+import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, CommonModule],
+  imports: [IonContent, CommonModule, PokemonCardComponent],
 })
 export class HomePage implements OnInit {
 

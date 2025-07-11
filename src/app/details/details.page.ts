db70adebeeb5bb0c6786b376fc4ea89761ba3518
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-07-10 21:22:26 -0300

    feat: add go back button and loading spinner to Pokémon details page

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,23 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
 import { ActivatedRoute } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MatProgressSpinnerModule]
 })
 export class DetailsPage implements OnInit {
 
   pokemonDetails: any = null;
   pokemonId: string | null = null;
+  loading: boolean = false;
 
-  constructor(private route: ActivatedRoute, private dataService: DataService) { }
+  constructor(private route: ActivatedRoute, private dataService: DataService, private location: Location) { }
 
   ngOnInit() {
     this.pokemonId = this.route.snapshot.paramMap.get('id');
@@ -29,6 +31,7 @@ export class DetailsPage implements OnInit {
   }
 
   getPokemonDetails(pokemonId: string) {
+    this.loading = true;
     this.dataService.getPokemonDetails(pokemonId).subscribe({
       next: (data: any) => {
         this.pokemonDetails = data;
@@ -36,8 +39,15 @@ export class DetailsPage implements OnInit {
       },
       error: (error) => {
         console.error("Erro ao buscar detalhes do pokemon: ", error);
+      },
+      complete: () => {
+        this.loading = false;
       }
     });
+  }
+
+  voltar(): void {
+    this.location.back();
   }
 
 }
